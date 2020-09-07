7e302e5e8557f19ea620ee3f9aa3485b7f649795
hashrock <hashrock@users.noreply.github.com>
2020-09-07 21:30:15 +0900

    feat: add floodfill

--- front/App.vue
+++ front/App.vue
@@ -1,13 +1,7 @@
 <template>
   <div>
     <div class="wrapper">
-      <canvas
-        class="bg"
-        :style="canvasStyle"
-        ref="canvasBg"
-        width="200"
-        height="200"
-       />
+      <canvas class="bg" :style="canvasStyle" ref="canvasBg" width="200" height="200" />
       <canvas
         class="canvasMain"
         :style="canvasStyle"
@@ -21,11 +15,20 @@
     </div>
     <div>
       <input type="color" v-model="selectedColor" />
-      <button class="palette" v-for="c in colors" :key="c" :style="{backgroundColor: `${c}`}" @click="selectedColor = c"></button>
+      <button
+        class="palette"
+        v-for="c in colors"
+        :key="c"
+        :style="{backgroundColor: `${c}`}"
+        @click="selectedColor = c"
+      ></button>
     </div>
     <div>
       <label>
-        <input type="checkbox" v-model="eraser">Eraser
+        <input type="checkbox" v-model="eraser" />Eraser
+      </label>
+      <label>
+        <input type="checkbox" v-model="flood" />flood
       </label>
       <select v-model.number="scale">
         <option value="1">x1</option>
@@ -39,7 +42,7 @@
         <option value="1">w1</option>
         <option value="2">w2</option>
         <option value="3">w3</option>
-      </select>      
+      </select>
     </div>
   </div>
 </template>
@@ -49,6 +52,9 @@ const vscode = acquireVsCodeApi();
 let canvas;
 let ctx;
 let imageData;
+import * as FloodFill from "./flood";
+import * as color from "./color"
+
 const colors = [
   "#000000",
   "#9d9d9d",
@@ -115,7 +121,8 @@ export default {
       width: 200,
       height: 200,
       colors: colors,
-      strokeWidth: 1
+      strokeWidth: 1,
+      flood: false
     };
   },
   computed: {
@@ -136,8 +143,8 @@ export default {
           if (body.untitled) {
             canvas.height = 32;
             canvas.width = 32;
-            ctx.clearRect(0, 0, 32, 32)
-            this.setupCanvasBg(32, 32)
+            ctx.clearRect(0, 0, 32, 32);
+            this.setupCanvasBg(32, 32);
             return;
           } else {
             // Load the initial image into the canvas.
@@ -174,24 +181,24 @@ export default {
     vscode.postMessage({ type: "ready" });
   },
   methods: {
-    setupCanvasBg(width, height){
-      const canvas = this.$refs.canvasBg
-      const ctx = canvas.getContext("2d")
-      canvas.width = width
-      canvas.height = height
+    setupCanvasBg(width, height) {
+      const canvas = this.$refs.canvasBg;
+      const ctx = canvas.getContext("2d");
+      canvas.width = width;
+      canvas.height = height;
 
       const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-      for(let y = 0; y < imageData.height; y++){
-        for(let x = 0; x < imageData.width; x++){
+      for (let y = 0; y < imageData.height; y++) {
+        for (let x = 0; x < imageData.width; x++) {
           const start = (y * imageData.width + x) * 4;
-          let bright = x % 2 === 0
-          if(y % 2 === 0){
-            bright = !bright
+          let bright = x % 2 === 0;
+          if (y % 2 === 0) {
+            bright = !bright;
           }
           imageData.data[start] = bright ? 200 : 180;
-          imageData.data[start+1] = bright ? 200 : 180;
-          imageData.data[start+2] = bright ? 200 : 180;
-          imageData.data[start+3] = 255;
+          imageData.data[start + 1] = bright ? 200 : 180;
+          imageData.data[start + 2] = bright ? 200 : 180;
+          imageData.data[start + 3] = 255;
         }
       }
       ctx.putImageData(imageData, 0, 0);
@@ -209,26 +216,47 @@ export default {
         imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
         this.ready = true;
 
-        this.setupCanvasBg(canvas.width, canvas.height)
+        this.setupCanvasBg(canvas.width, canvas.height);
         this.redraw();
       }
     },
     down(ev) {
-      canvas.setPointerCapture(ev.pointerId);
-      this.drag = true;
-      this.old = {
-        x: round(ev.offsetX),
-        y: round(ev.offsetY)
-      };
-      this.move(ev);
+      if (ev.button === 0) {
+        if (this.flood) {
+          this.startFlood(round(ev.offsetX), round(ev.offsetY));
+        } else {
+          canvas.setPointerCapture(ev.pointerId);
+          this.drag = true;
+          this.old = {
+            x: round(ev.offsetX),
+            y: round(ev.offsetY)
+          };
+          this.move(ev);
+        }
+      }
+      if (ev.button === 2) {
+        const pixel = this.getPixel(round(ev.offsetX), round(ev.offsetY));
+        this.selectedColor =
+          "#" +
+          parseInt(pixel[0]).toString(16) +
+          parseInt(pixel[1]).toString(16) +
+          parseInt(pixel[2]).toString(16);
+        if (pixel[3] === 0) {
+          this.eraser = true;
+        } else {
+          this.eraser = false;
+        }
+      }
     },
-    async up() {
-      this.drag = false;
+    async up(ev) {
+      if (this.drag) {
+        this.drag = false;
 
-      vscode.postMessage({
-        type: "update",
-        snapshot: await getImageData()
-      });
+        vscode.postMessage({
+          type: "update",
+          snapshot: await getImageData()
+        });
+      }
     },
     move(ev) {
       if (this.drag) {
@@ -251,25 +279,25 @@ export default {
       let err = dx - dy;
 
       while (true) {
-        if(this.strokeWidth === 1){
+        if (this.strokeWidth === 1) {
           this.setPixel(x0, y0);
         }
-        if(this.strokeWidth === 2){
+        if (this.strokeWidth === 2) {
           this.setPixel(x0, y0);
-          this.setPixel(x0+1, y0);
-          this.setPixel(x0, y0+1);
-          this.setPixel(x0+1, y0+1);
+          this.setPixel(x0 + 1, y0);
+          this.setPixel(x0, y0 + 1);
+          this.setPixel(x0 + 1, y0 + 1);
         }
-        if(this.strokeWidth === 3){
-          this.setPixel(x0-1, y0-1);
-          this.setPixel(x0-1, y0);
-          this.setPixel(x0-1, y0+1);
-          this.setPixel(x0, y0-1);
+        if (this.strokeWidth === 3) {
+          this.setPixel(x0 - 1, y0 - 1);
+          this.setPixel(x0 - 1, y0);
+          this.setPixel(x0 - 1, y0 + 1);
+          this.setPixel(x0, y0 - 1);
           this.setPixel(x0, y0);
-          this.setPixel(x0, y0+1);
-          this.setPixel(x0+1, y0-1);
-          this.setPixel(x0+1, y0);
-          this.setPixel(x0+1, y0+1);
+          this.setPixel(x0, y0 + 1);
+          this.setPixel(x0 + 1, y0 - 1);
+          this.setPixel(x0 + 1, y0);
+          this.setPixel(x0 + 1, y0 + 1);
         }
 
         if (x0 == x1 && y0 == y1) {
@@ -301,20 +329,65 @@ export default {
       }
 
       const start = (y * this.width + x) * 4;
-      if(this.eraser){
+      if (this.eraser) {
         imageData.data[start] = 0;
         imageData.data[start + 1] = 0;
         imageData.data[start + 2] = 0;
         imageData.data[start + 3] = 0;
-      }else{
+      } else {
         imageData.data[start] = parseInt(this.selectedColor.slice(1, 3), 16);
-        imageData.data[start + 1] = parseInt(this.selectedColor.slice(3, 5), 16);
-        imageData.data[start + 2] = parseInt(this.selectedColor.slice(5, 7), 16);
+        imageData.data[start + 1] = parseInt(
+          this.selectedColor.slice(3, 5),
+          16
+        );
+        imageData.data[start + 2] = parseInt(
+          this.selectedColor.slice(5, 7),
+          16
+        );
         imageData.data[start + 3] = 255;
       }
+    },
+    getPixel(x, y) {
+      if (x < 0 || y < 0 || x > imageData.width || y > imageData.height) {
+        return [-1, -1, -1, -1];
+      }
+      const start = (y * this.width + x) * 4;
+      return [
+        imageData.data[start],
+        imageData.data[start + 1],
+        imageData.data[start + 2],
+        imageData.data[start + 3]
+      ];
+    },
+    async startFlood(x, y) {
+      const fillColor = color.colorToRGBA(this.selectedColor)
+
+      FloodFill.floodfill(
+        imageData.data,
+        x,
+        y,
+        {
+          r: fillColor[0],
+          g: fillColor[1],
+          b: fillColor[2],
+          a: fillColor[3]
+        },
+        0,
+        this.width,
+        this.height
+      );
+      vscode.postMessage({
+        type: "update",
+        snapshot: await getImageData()
+      });
+      this.redraw();
     }
   }
 };
+
+function equalsColor(a, b) {
+  return a[0] === b[0] && a[1] === b[1] && a[2] === b[2] && a[3] === b[3];
+}
 </script>
 
 <style>
@@ -335,7 +408,7 @@ canvas {
   overflow: scroll;
 }
 
-.palette{
+.palette {
   width: 16px;
   height: 16px;
   border: none;
